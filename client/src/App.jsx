099903dba688958fdfa12534b87dc48655693014
Nicholaschibueze <nicholaschibuezemichael@gmail.com>
2023-10-31 16:40:43 +0100

    worked on profile component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
 import Home from './pages/Home';
-import SignIn from './pages/SignIn';
 import SignUp from './pages/signUp';
+import Signin from './pages/SignIn';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
-import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/about' element={<About />} />
-        <Route element={<PrivateRoute />}>
-          <Route path='/profile' element={<Profile />} />
-          
-        </Route>
-      </Routes>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/sign-in' element={<Signin/>}></Route>
+      <Route path='/sign-up' element={<SignUp/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/profile' element={<Profile/>}>
+
+      </Route>
+    </Routes>
     </BrowserRouter>
-  );
+  )
 }
