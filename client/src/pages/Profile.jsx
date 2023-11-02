51305483b0341573d86a91ebc330600dbb920985
Nicholaschibueze <nicholaschibuezemichael@gmail.com>
2023-11-02 11:38:16 +0100

    complete update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,14 +4,18 @@ import { useSelector } from 'react-redux';
 // eslint-disable-next-line no-unused-vars
 import  {getDownloadURL, getStorage, ref, uploadBytes, uploadBytesResumable} from 'firebase/storage';
 import { app } from '../firebase';
+import { updateUserStart, updateUserFailure, updateUserSuccess } from '../redux/user/userSlice.js';
+import { useDispatch } from 'react-redux';
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined); 
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
 
 
 
@@ -52,12 +56,44 @@ export default function Profile() {
      
   };
 
+  const handleChange = async  (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value});
+  };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+
+      });
+
+      const data = await res.json();
+      if (data.success === false){
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+
+      setUpdateSuccess(true);
+      
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+      
+    }
+
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
 
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           onChange={handleFileChange}
           type='file'
@@ -67,7 +103,7 @@ export default function Profile() {
         />
         <img
           onClick={() => fileRef.current.click()}
-          src={formData.avatar || currentUser.avatar}
+          src={formData?.avatar || currentUser.avatar}
           alt='profile'
           className='rounded-full h-24 w-24
            object-cover 
@@ -90,15 +126,19 @@ export default function Profile() {
         </p>
 
         <input type='text'
-         placeholder='username' 
+         placeholder='username'
+         defaultValue={currentUser.username} 
         className='border
          p-3 rounded-lg'
+         onChange={handleChange}
           id='username' />
 
         <input type='email' 
         placeholder='email'
+        defaultValue={currentUser.email}
          className='border
           p-3 rounded-lg'
+          onChange={handleChange}
            id='email' />
 
         <input type='password'
@@ -107,17 +147,21 @@ export default function Profile() {
            p-3 rounded-lg'
             id='password' />
 
-        <button className='bg-slate-700
+        <button disabled={loading} className='bg-slate-700
          text-white 
          rounded-lg p-3
           uppercase hover:opacity-95
            disabled:opacity-80'>
-            update</button>
+            {loading ? 'Loading...' : 'Update'}
+        </button>
       </form>
       <div className='flex justify-between mt-5'>
         <span className='text-red-700 cursor-pointer'>Delete Account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
+
+      <p className='text-red-700 mt-5'>{error ?  error : ''}</p>
+      <p className='text-green-700 mt-5'>{updateSuccess ? 'User is updated successfully' :  ''}</p>
     </div>
   );
 }
